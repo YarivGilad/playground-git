a46cd2230cadf154d4dfe9fc7523953c3f88307a
Yariv Gilad <yariv.gilad@gmail.com>
2023-02-12 18:49:09 +0200

    abracadabra is modified to color logs

---
+++ some.mjs
@@ -0,0 +1,11 @@
+import log from '@ajar/marker';
+
+
+log.blue('blue','ocean');       
+log.magenta('magenta','is lighter then purple');    
+log.cyan('cyan','is lighter then blue');       
+log.green('green','peace');      
+log.yellow('yellow','mellow');     
+log.red('red','tape'); 
+
+
